b3ee9a8c509b400abef2cdee7ae0ec8388868dd4
Tao <1747290216@qq.com>
2025-08-13 17:47:24 +0800

    修改tab

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: ['chartjs-plugin-zoom'],
+  
+  // 设置页面标题
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Visualizer',
+    },
+  },
 
   chainWebpack: config => {
     config.module
